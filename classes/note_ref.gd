f91eb1a5df723fcfa519afd787b7f5012ea452c4
lxviolet <>
2026-04-05 14:54:47 +0100

    i forgot

--- classes/note_ref.gd
+++ classes/note_ref.gd
@@ -6,7 +6,6 @@ var color: NoteInfo.NoteColor = NoteInfo.NoteColor.WHITE
 var timestamp: float
 var id: String
 
-#func _init(lane_: NoteInfo.NoteLane, color_: NoteInfo.NoteColor, timestamp_: float) -> void:
 func _init(note_info: NoteInfo) -> void:
 	self.lane = note_info.lane
 	self.color = note_info.color
